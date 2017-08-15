3f424a0069671d22dbac054d9dd74cff179fd1b0
Renzo Orsini <orsini@unive.it>
2017-08-15 11:27:32 +0200

    managed declarations and documentation of a function

--- memoize.lisp
+++ memoize.lisp
@@ -1,33 +1,44 @@
 
 
 (defmacro defmem(name args &body body)
-  (let ((ll (length args))
-	(val-name (gensym))
-	(val-val (gensym))
-	(t-name (gensym))
-	(res-name (gensym))
-	(pre-name (gensym)))
-    (cond ((= ll 0)                      ; no arguments
-	   `(let ((,val-name ',val-val))
-	      (defun ,name ,args
-		(if (eq ,val-name ',val-val)
-		    (setf ,val-name (progn ,@body))
-		    ,val-name))))
-	  ((= ll 1)                            ; one argument	       
-	   `(let ((,t-name (make-hash-table :test 'equal)))
-	      (defun ,name (,@args)
-		(multiple-value-bind (,res-name ,pre-name)
-		    (gethash ,@args ,t-name)
-		  (if ,pre-name
-		      ,res-name
-		      (setf (gethash ,@args ,t-name)
-			    (progn ,@body)))))))
-	  (t                                  ; multiple arguments
-	   `(let ((,t-name (make-hash-table :test 'equal)))
-	      (defun ,name ,args
-		(multiple-value-bind (,res-name ,pre-name)
-		    (gethash (list ,@args) ,t-name)
-		  (if ,pre-name
-		      ,res-name
-		      (setf (gethash (list ,@args) ,t-name)
-			    (progn ,@body))))))))))
+  (let* ((ll (length args))
+	 (val-name (gensym))
+	 (val-val (gensym))
+	 (t-name (gensym))
+	 (res-name (gensym))
+	 (pre-name (gensym)))
+    (multiple-value-bind (decs-docs real-body)
+	 (loop for (b . r) on body
+	       while (or (and (listp b) (eq (car b) 'declare))
+			 (and (stringp b) r))
+	       collect b into d-d
+	       finally (return (values d-d (or r (list b)))))
+      (cond ((= ll 0)                      ; no arguments
+	     `(let ((,val-name ',val-val))
+		(defun ,name ,args ,@decs-docs
+		  (if (eq ,val-name ',val-val)
+		      (setf ,val-name (progn ,@real-body))
+		      ,val-name))))
+	    ((= ll 1)                            ; one argument	       
+	     `(let ((,t-name (make-hash-table :test 'equal)))
+		(defun ,name (,@args) ,@decs-docs
+		  (multiple-value-bind (,res-name ,pre-name)
+		      (gethash ,@args ,t-name)
+		    (if ,pre-name
+			,res-name
+			(setf (gethash ,@args ,t-name)
+			      (progn ,@real-body)))))))
+	    (t                                  ; multiple arguments
+	     `(let ((,t-name (make-hash-table :test 'equal)))
+		(defun ,name ,args ,@decs-docs
+		  (multiple-value-bind (,res-name ,pre-name)
+		      (gethash (list ,@args) ,t-name)
+		    (if ,pre-name
+			,res-name
+			(setf (gethash (list ,@args) ,t-name)
+			      (progn ,@real-body)))))))))))
+
+
+;; example:
+
+; (defmem fibo (n) (if (< n 2) 1 (+ (fibo (- n 1)) (fibo (- n 2)))))
